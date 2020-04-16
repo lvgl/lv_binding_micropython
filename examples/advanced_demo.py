a28869f7ce6b466b0da4e0087e9baa93d72d2a55
prolomb <prospero.lombardi@polimi.it>
2020-04-16 14:01:08 +0200

    /examples/ updated examples for STM32F7DISC

--- examples/advanced_demo.py
+++ examples/advanced_demo.py
@@ -12,15 +12,17 @@ symbolstyle.text.font = lv.font_roboto_28
 # The following two lines do the same thing.
 # They show how to initialize struct either directly or through a dict
 
-symbolstyle.text.color = lv.color_hex(0xffffff)
-symbolstyle.text.color = lv.color_make(0xff, 0xff, 0xff)
-if hasattr(symbolstyle.text.color.ch, 'alpha'):
-    symbolstyle.text.color.ch.alpha = 0xff # Only has alpha when color is 32 bit
+symbolstyle.text.color = lv.color_hex(0xFFFFFF)
+symbolstyle.text.color = lv.color_make(0xFF, 0xFF, 0xFF)
+if hasattr(symbolstyle.text.color.ch, "alpha"):
+    symbolstyle.text.color.ch.alpha = 0xFF  # Only has alpha when color is 32 bit
+
 
 def get_member_name(obj, value):
     for member in dir(obj):
         if getattr(obj, member) == value:
             return member
+
 
 class SymbolButton(lv.btn):
     def __init__(self, parent, symbol, text):
@@ -28,90 +30,103 @@ class SymbolButton(lv.btn):
         self.symbol = lv.label(self)
         self.symbol.set_text(symbol)
         self.symbol.set_style(lv.label.STYLE.MAIN, symbolstyle)
-        
+
         self.label = lv.label(self)
         self.label.set_text(text)
-        
+
+
 class Page_Buttons:
     def __init__(self, app, page):
         self.app = app
         self.page = page
 
         self.btn1 = SymbolButton(page, lv.SYMBOL.PLAY, "Play")
-        self.btn1.set_size(80,80)
+        self.btn1.set_size(80, 80)
         self.btn1.align(page, lv.ALIGN.IN_TOP_LEFT, 30, 30)
-        
+
         self.btn2 = SymbolButton(page, lv.SYMBOL.PAUSE, "Pause")
-        self.btn2.set_size(80,80)
+        self.btn2.set_size(80, 80)
         self.btn2.align(page, lv.ALIGN.IN_TOP_RIGHT, -30, 30)
-    
+
         self.label = lv.label(page)
         self.label.align(page, lv.ALIGN.IN_BOTTOM_LEFT, 30, -30)
 
         # Currently only single callback per object is supported
 
-        for btn, name in [(self.btn1, 'Play'), (self.btn2, 'Pause')]:
-            btn.set_event_cb(lambda obj=None, event=-1, name=name: self.label.set_text('%s %s' % (name, get_member_name(lv.EVENT, event))))
+        for btn, name in [(self.btn1, "Play"), (self.btn2, "Pause")]:
+            btn.set_event_cb(
+                lambda obj=None, event=-1, name=name: self.label.set_text(
+                    "%s %s" % (name, get_member_name(lv.EVENT, event))
+                )
+            )
 
 
 class Page_Simple:
     def __init__(self, app, page):
         self.app = app
         self.page = page
-        
-        # slider 
+
+        # slider
         self.slider = lv.slider(page)
         self.slider.align(page, lv.ALIGN.IN_TOP_LEFT, 20, 20)
         self.slider_label = lv.label(page)
         self.slider_label.align(self.slider, lv.ALIGN.OUT_RIGHT_MID, 15, 0)
         self.slider.set_event_cb(self.on_slider_changed)
         self.on_slider_changed(None)
-        
+
         # style selector
-        self.styles = [('Plain', lv.style_plain), ('Plain color', lv.style_plain_color), ('Pretty', lv.style_pretty), ('Pretty color', lv.style_pretty_color)]
-    
+        self.styles = [
+            ("Plain", lv.style_plain),
+            ("Plain color", lv.style_plain_color),
+            ("Pretty", lv.style_pretty),
+            ("Pretty color", lv.style_pretty_color),
+        ]
+
         self.style_selector = lv.ddlist(page)
         self.style_selector.align(self.slider, lv.ALIGN.IN_BOTTOM_LEFT, 0, 40)
-        self.style_selector.set_options('\n'.join(x[0] for x in self.styles))
+        self.style_selector.set_options("\n".join(x[0] for x in self.styles))
         self.style_selector.set_event_cb(self.on_style_selector_changed)
 
         # counter button
         self.counter_btn = lv.btn(page)
-        self.counter_btn.set_size(80,80)
+        self.counter_btn.set_size(80, 80)
         self.counter_btn.align(self.style_selector, lv.ALIGN.OUT_RIGHT_TOP, 40, 0)
         self.counter_label = lv.label(self.counter_btn)
-        self.counter_label.set_text('Count')
+        self.counter_label.set_text("Count")
         self.counter_btn.set_event_cb(self.on_counter_btn)
         self.counter = 0
 
-    
     def on_slider_changed(self, obj=None, event=-1):
         self.slider_label.set_text(str(self.slider.get_value()))
 
     def on_style_selector_changed(self, obj=None, event=-1):
         selected = self.style_selector.get_selected()
-        self.app.screen_main.tabview.set_style(lv.tabview.STYLE.BG, self.styles[selected][1])   
+        self.app.screen_main.tabview.set_style(lv.tabview.STYLE.BG, self.styles[selected][1])
 
     def on_counter_btn(self, obj, event):
         if event == lv.EVENT.CLICKED:
             self.counter += 1
             self.counter_label.set_text(str(self.counter))
 
+
 class Anim(lv.anim_t):
-    def __init__(self, obj, val, size, exec_cb, path_cb, time=500, playback = False, ready_cb=None):
+    def __init__(self, obj, val, size, exec_cb, path_cb, time=500, playback=False, ready_cb=None):
         super().__init__()
         lv.anim_init(self)
         lv.anim_set_time(self, time, 0)
-        lv.anim_set_values(self, val, val+size)
+        lv.anim_set_values(self, val, val + size)
         if callable(exec_cb):
             lv.anim_set_custom_exec_cb(self, exec_cb)
         else:
             lv.anim_set_exec_cb(self, obj, exec_cb)
-        lv.anim_set_path_cb(self, path_cb )
-        if playback: lv.anim_set_playback(self, 0)
-        if ready_cb: lv.anim_set_ready_cb(self, ready_cb)
+        lv.anim_set_path_cb(self, path_cb)
+        if playback:
+            lv.anim_set_playback(self, 0)
+        if ready_cb:
+            lv.anim_set_ready_cb(self, ready_cb)
         lv.anim_create(self)
-        
+
+
 class AnimatedChart(lv.chart):
     def __init__(self, parent, val, size):
         super().__init__(parent)
@@ -129,21 +144,23 @@ class AnimatedChart(lv.chart):
             self.size,
             lambda a, val: self.set_range(0, val),
             lv.anim_path_ease_in,
-            ready_cb=lambda a:self.anim_phase2(),
-            time=(self.max * self.factor) // 100)
+            ready_cb=lambda a: self.anim_phase2(),
+            time=(self.max * self.factor) // 100,
+        )
 
     def anim_phase2(self):
         Anim(
             self,
-            self.val+self.size,
+            self.val + self.size,
             -self.size,
             lambda a, val: self.set_range(0, val),
             lv.anim_path_ease_out,
-            ready_cb=lambda a:self.anim_phase1(),
-            time=(self.min * self.factor) // 100)
+            ready_cb=lambda a: self.anim_phase1(),
+            time=(self.min * self.factor) // 100,
+        )
 
 
-class Page_Chart():
+class Page_Chart:
     def __init__(self, app, page):
         self.app = app
         self.page = page
@@ -153,12 +170,12 @@ class Page_Chart():
         self.series1 = self.chart.add_series(lv.color_hex(0xFF0000))
         self.chart.set_type(self.chart.TYPE.POINT | self.chart.TYPE.LINE)
         self.chart.set_series_width(3)
-        self.chart.set_range(0,100)
+        self.chart.set_range(0, 100)
         self.chart.init_points(self.series1, 10)
-        self.chart.set_points(self.series1, [10,20,30,20,10,40,50,90,95,90])
-        self.chart.set_x_tick_texts('a\nb\nc\nd\ne', 2, lv.chart.AXIS.DRAW_LAST_TICK)
+        self.chart.set_points(self.series1, [10, 20, 30, 20, 10, 40, 50, 90, 95, 90])
+        self.chart.set_x_tick_texts("a\nb\nc\nd\ne", 2, lv.chart.AXIS.DRAW_LAST_TICK)
         self.chart.set_x_tick_length(10, 5)
-        self.chart.set_y_tick_texts('1\n2\n3\n4\n5', 2, lv.chart.AXIS.DRAW_LAST_TICK)
+        self.chart.set_y_tick_texts("1\n2\n3\n4\n5", 2, lv.chart.AXIS.DRAW_LAST_TICK)
         self.chart.set_y_tick_length(10, 5)
         self.chart.set_div_line_count(3, 3)
         self.chart.set_margin(30)
@@ -176,29 +193,30 @@ class Page_Chart():
         self.slider.set_value(self.chart.factor, 0)
         self.slider.set_event_cb(on_slider_changed)
 
-        
+
 class Screen_Main(lv.obj):
     def __init__(self, app, *args, **kwds):
         self.app = app
         super().__init__(*args, **kwds)
-        
+
         self.tabview = lv.tabview(self)
-        self.page_simple = Page_Simple(self.app, self.tabview.add_tab('Simple'))
-        self.page_buttons = Page_Buttons(self.app, self.tabview.add_tab('Buttons'))
-        self.page_chart = Page_Chart(self.app, self.tabview.add_tab('Chart'))
+        self.page_simple = Page_Simple(self.app, self.tabview.add_tab("Simple"))
+        self.page_buttons = Page_Buttons(self.app, self.tabview.add_tab("Buttons"))
+        self.page_chart = Page_Chart(self.app, self.tabview.add_tab("Chart"))
 
 
-class AdvancedDemoApplication():
+class AdvancedDemoApplication:
     def init_gui_SDL(self):
 
         import SDL
+
         SDL.init()
 
         # Register SDL display driver.
 
         disp_buf1 = lv.disp_buf_t()
-        buf1_1 = bytes(480*10)
-        lv.disp_buf_init(disp_buf1,buf1_1, None, len(buf1_1)//4)
+        buf1_1 = bytes(480 * 10)
+        lv.disp_buf_init(disp_buf1, buf1_1, None, len(buf1_1) // 4)
         disp_drv = lv.disp_drv_t()
         lv.disp_drv_init(disp_drv)
         disp_drv.buffer = disp_buf1
@@ -210,11 +228,11 @@ class AdvancedDemoApplication():
         # Regsiter SDL mouse driver
 
         indev_drv = lv.indev_drv_t()
-        lv.indev_drv_init(indev_drv) 
-        indev_drv.type = lv.INDEV_TYPE.POINTER;
-        indev_drv.read_cb = SDL.mouse_read;
-        lv.indev_drv_register(indev_drv);
-        
+        lv.indev_drv_init(indev_drv)
+        indev_drv.type = lv.INDEV_TYPE.POINTER
+        indev_drv.read_cb = SDL.mouse_read
+        lv.indev_drv_register(indev_drv)
+
     def init_gui_esp32(self):
 
         import lvesp32
@@ -222,11 +240,12 @@ class AdvancedDemoApplication():
         # Initialize ILI9341 display
 
         from ili9341 import ili9341
+
         self.disp = ili9341(dc=32, cs=33, power=-1, backlight=-1)
 
         # Register raw resistive touch driver
 
-        '''
+        """
         import rtch
         self.touch = rtch.touch(xp = 32, yp = 33, xm = 25, ym = 26, touch_rail = 27, touch_sense = 33)
         self.touch.init()
@@ -235,24 +254,63 @@ class AdvancedDemoApplication():
         indev_drv.type = lv.INDEV_TYPE.POINTER;
         indev_drv.read_cb = self.touch.read;
         lv.indev_drv_register(indev_drv);
-        '''
+        """
 
         # Register xpt2046 touch driver
 
         from xpt2046 import xpt2046
+
         self.touch = xpt2046()
-    
+
+    def init_gui_stm32(self):
+        import rk043fn48h as lcd
+        import lvstm32
+
+        hres = 480
+        vres = 272
+
+        # Register display driver
+        tick = lvstm32.lvstm32(freq=30)
+        lcd.init(w=hres, h=vres)
+        disp_buf1 = lv.disp_buf_t()
+        buf1_1 = lcd.bytearray(hres * vres * lv.color_t.SIZE)
+        buf1_2 = lcd.bytearray(hres * vres * lv.color_t.SIZE)
+        # buf1_1 = lcd.bytearray(hres*10*lv.color_t.SIZE)
+        # buf1_2 = lcd.bytearray(hres*10*lv.color_t.SIZE)
+        lv.disp_buf_init(disp_buf1, buf1_1, buf1_2, len(buf1_1) // lv.color_t.SIZE)
+        disp_drv = lv.disp_drv_t()
+        lv.disp_drv_init(disp_drv)
+        disp_drv.buffer = disp_buf1
+        disp_drv.flush_cb = lcd.flush
+        disp_drv.gpu_blend_cb = lcd.gpu_blend
+        disp_drv.gpu_fill_cb = lcd.gpu_fill
+        disp_drv.hor_res = hres
+        disp_drv.ver_res = vres
+        lv.disp_drv_register(disp_drv)
+
+        # Register touch sensor
+        indev_drv = lv.indev_drv_t()
+        lv.indev_drv_init(indev_drv)
+        indev_drv.type = lv.INDEV_TYPE.POINTER
+        indev_drv.read_cb = lcd.ts_read
+        lv.indev_drv_register(indev_drv)
+
     def init_gui(self):
-        
+
         # Identify platform and initialize it
 
         try:
             self.init_gui_esp32()
         except ImportError:
             pass
-    
+
         try:
             self.init_gui_SDL()
+        except ImportError:
+            pass
+
+        try:
+            self.init_gui_stm32()
         except ImportError:
             pass
 
@@ -261,10 +319,10 @@ class AdvancedDemoApplication():
         self.screen_main = Screen_Main(self)
         lv.scr_load(self.screen_main)
 
+
 app = AdvancedDemoApplication()
 app.init_gui()
 
 # if __name__ == '__main__':
 #    while True:
 #        pass
-
