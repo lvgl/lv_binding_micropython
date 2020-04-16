607c3d56e2d13bf3b995339e3eb39ea43f3fdac3
prolomb <prospero.lombardi@polimi.it>
2020-04-16 14:01:08 +0200

    /examples/ updated examples for STM32F7DISC

--- examples/chart_anim_example.py
+++ examples/chart_anim_example.py
@@ -1,17 +1,17 @@
-
-
 # init
 
 import lvgl as lv
+
 lv.init()
 
-class driver:
 
-    def init_disp_drv(self, flush, hor_res, ver_res, buf_size = 0):
+class driver:
+    def init_disp_drv(self, flush, hor_res, ver_res, buf_size=0):
         disp_buf1 = lv.disp_buf_t()
-        if not buf_size: buf_size = (hor_res*ver_res)//4
+        if not buf_size:
+            buf_size = (hor_res * ver_res) // 4
         buf1_1 = bytearray(buf_size)
-        lv.disp_buf_init(disp_buf1,buf1_1, None, len(buf1_1)//4)
+        lv.disp_buf_init(disp_buf1, buf1_1, None, len(buf1_1) // lv.color_t.SIZE)
         disp_drv = lv.disp_drv_t()
         lv.disp_drv_init(disp_drv)
         disp_drv.buffer = disp_buf1
@@ -22,17 +22,17 @@ class driver:
 
     def init_indev_drv(self, mouse_read):
         indev_drv = lv.indev_drv_t()
-        lv.indev_drv_init(indev_drv) 
-        indev_drv.type = lv.INDEV_TYPE.POINTER;
+        lv.indev_drv_init(indev_drv)
+        indev_drv.type = lv.INDEV_TYPE.POINTER
         indev_drv.read_cb = mouse_read
-        lv.indev_drv_register(indev_drv);
- 
+        lv.indev_drv_register(indev_drv)
 
     def init_gui_fb(self):
         import fb
+
         fb.init()
         if self.init_disp_drv(fb.flush, 480, 320):
-            print('Initialized Linux Frame Buffer device')
+            print("Initialized Linux Frame Buffer device")
             return True
         else:
             fb.deinit()
@@ -40,28 +40,42 @@ class driver:
 
     def init_gui_SDL(self):
         import SDL
+
         SDL.init()
         self.init_disp_drv(SDL.monitor_flush, 480, 320)
         self.init_indev_drv(SDL.mouse_read)
-        print('Initialized SDL device')
+        print("Initialized SDL device")
         return True
-        
+
     def init_gui_esp32(self):
         import lvesp32
         import ILI9341 as ili
+
         disp = ili.display(miso=5, mosi=18, clk=19, cs=13, dc=12, rst=4, backlight=15)
         disp.init()
         self.init_disp_drv(disp.flush, 240, 320)
         import rtch
-        touch = rtch.touch(xp = 32, yp = 33, xm = 25, ym = 26, touch_rail = 27, touch_sense = 33)
+
+        touch = rtch.touch(xp=32, yp=33, xm=25, ym=26, touch_rail=27, touch_sense=33)
         touch.init()
         self.init_indev_drv(touch_read)
-        print('Initialized ILI9341 device')
+        print("Initialized ILI9341 device")
         return True
 
-    
+    def init_gui_stm32(self):
+        import rk043fn48h as lcd
+        import lvstm32
+
+        hres = 480
+        vres = 272
+        tick = lvstm32.lvstm32(freq=30)
+        lcd.init(w=hres, h=vres)
+        self.init_disp_drv(lcd.flush, hres, vres)
+        self.init_indev_drv(lcd.ts_read)
+        return True
+
     def init_gui(self):
-        
+
         # Identify platform and initialize it
 
         try:
@@ -69,43 +83,54 @@ class driver:
                 return
         except ImportError:
             pass
-    
+
         # try:
         #     if self.init_gui_fb():
         #         return
         # except ImportError:
         #     pass
- 
+
         try:
             if self.init_gui_SDL():
                 return
         except ImportError:
             pass
 
+        try:
+            self.init_gui_stm32()
+        except ImportError:
+            pass
+
+
 drv = driver()
 drv.init_gui()
 
 # Animation helper class
 
+
 class Anim(lv.anim_t):
-    def __init__(self, obj, val, size, exec_cb, path_cb, time=500, playback = False, ready_cb=None):
+    def __init__(self, obj, val, size, exec_cb, path_cb, time=500, playback=False, ready_cb=None):
         super().__init__()
         lv.anim_init(self)
         lv.anim_set_time(self, time, 0)
-        lv.anim_set_values(self, val, val+size)
+        lv.anim_set_values(self, val, val + size)
         if callable(exec_cb):
             lv.anim_set_custom_exec_cb(self, exec_cb)
         else:
             lv.anim_set_exec_cb(self, obj, exec_cb)
-        lv.anim_set_path_cb(self, path_cb )
-        if playback: lv.anim_set_playback(self, 0)
-        if ready_cb: lv.anim_set_ready_cb(self, ready_cb)
+        lv.anim_set_path_cb(self, path_cb)
+        if playback:
+            lv.anim_set_playback(self, 0)
+        if ready_cb:
+            lv.anim_set_ready_cb(self, ready_cb)
         lv.anim_create(self)
+
 
 # An animated chart
 
+
 class AnimatedChart(lv.chart):
-    def __init__(self, parent, val, size):            
+    def __init__(self, parent, val, size):
         super().__init__(parent)
         self.val = val
         self.size = size
@@ -116,23 +141,26 @@ class AnimatedChart(lv.chart):
 
     def anim_phase1(self):
         Anim(
-            self, 
-            self.val, 
-            self.size, 
-            lambda a, val: self.set_range(0, val), 
-            lv.anim_path_ease_in, 
-            ready_cb=lambda a:self.anim_phase2(),
-            time=(self.max * self.factor) // 100)
+            self,
+            self.val,
+            self.size,
+            lambda a, val: self.set_range(0, val),
+            lv.anim_path_ease_in,
+            ready_cb=lambda a: self.anim_phase2(),
+            time=(self.max * self.factor) // 100,
+        )
 
     def anim_phase2(self):
         Anim(
-            self, 
-            self.val+self.size, 
-            -self.size, 
-            lambda a, val: self.set_range(0, val), 
-            lv.anim_path_ease_out, 
-            ready_cb=lambda a:self.anim_phase1(),
-            time=(self.min * self.factor) // 100)
+            self,
+            self.val + self.size,
+            -self.size,
+            lambda a, val: self.set_range(0, val),
+            lv.anim_path_ease_out,
+            ready_cb=lambda a: self.anim_phase1(),
+            time=(self.min * self.factor) // 100,
+        )
+
 
 # Create the chart
 
@@ -143,20 +171,22 @@ chart.align(scr, lv.ALIGN.CENTER, 0, 0)
 series1 = chart.add_series(lv.color_hex(0xFF0000))
 chart.set_type(chart.TYPE.POINT | chart.TYPE.LINE)
 chart.set_series_width(3)
-chart.set_range(0,100)
+chart.set_range(0, 100)
 chart.init_points(series1, 10)
-chart.set_points(series1, [10,20,30,20,10,40,50,90,95,90])
-chart.set_x_tick_texts('a\nb\nc\nd\ne', 2, lv.chart.AXIS.DRAW_LAST_TICK)
+chart.set_points(series1, [10, 20, 30, 20, 10, 40, 50, 90, 95, 90])
+chart.set_x_tick_texts("a\nb\nc\nd\ne", 2, lv.chart.AXIS.DRAW_LAST_TICK)
 chart.set_x_tick_length(10, 5)
-chart.set_y_tick_texts('1\n2\n3\n4\n5', 2, lv.chart.AXIS.DRAW_LAST_TICK)
+chart.set_y_tick_texts("1\n2\n3\n4\n5", 2, lv.chart.AXIS.DRAW_LAST_TICK)
 chart.set_y_tick_length(10, 5)
 chart.set_div_line_count(3, 3)
 chart.set_margin(30)
 
 # Create a slider that controls the chart animation speed
 
+
 def on_slider_changed(self, obj=None, event=-1):
     chart.factor = slider.get_value()
+
 
 slider = lv.slider(scr)
 slider.align(chart, lv.ALIGN.OUT_RIGHT_TOP, 10, 0)
@@ -169,4 +199,3 @@ slider.set_event_cb(on_slider_changed)
 # Load the screen
 
 lv.scr_load(scr)
-
---
+++ examples/example3.py
@@ -0,0 +1,136 @@
+import pyb
+import time
+import rk043fn48h as lcd
+import lvgl as lv
+import lvstm32
+
+hres = 480
+vres = 272
+lv.init()
+tick = lvstm32.lvstm32(freq=30)
+lcd.init(w=hres, h=vres)
+disp_buf1 = lv.disp_buf_t()
+# buf1_1 = bytearray(hres*vres*lv.color_t.SIZE)
+# buf1_2 = bytearray(hres*vres*lv.color_t.SIZE)
+buf1_1 = lcd.bytearray(hres * 10 * lv.color_t.SIZE)
+buf1_2 = lcd.bytearray(hres * 10 * lv.color_t.SIZE)
+lv.disp_buf_init(disp_buf1, buf1_1, buf1_2, len(buf1_1) // lv.color_t.SIZE)
+disp_drv = lv.disp_drv_t()
+lv.disp_drv_init(disp_drv)
+disp_drv.buffer = disp_buf1
+disp_drv.flush_cb = lcd.flush
+disp_drv.gpu_blend_cb = lcd.gpu_blend
+disp_drv.gpu_fill_cb = lcd.gpu_fill
+disp_drv.hor_res = hres
+disp_drv.ver_res = vres
+lv.disp_drv_register(disp_drv)
+
+indev_drv = lv.indev_drv_t()
+lv.indev_drv_init(indev_drv)
+indev_drv.type = lv.INDEV_TYPE.POINTER
+indev_drv.read_cb = lcd.ts_read
+lv.indev_drv_register(indev_drv)
+
+th = lv.theme_material_init(10, lv.font_roboto_16)
+# th = lv.theme_alien_init(160, lv.font_roboto_16)
+# th = lv.theme_zen_init(120, lv.font_roboto_16)
+# th = lv.theme_nemo_init(180, lv.font_roboto_16)
+lv.theme_set_current(th)
+
+scr1 = lv.obj()
+scr2 = lv.obj()
+lv.scr_load(scr1)
+
+slider = lv.slider(scr2)
+slider.set_width(200)
+slider.align(scr2, lv.ALIGN.IN_TOP_MID, 0, 15)
+
+btn1 = lv.btn(scr1)
+btn1.align(scr1, lv.ALIGN.IN_TOP_RIGHT, -5, 5)
+label = lv.label(btn1)
+label.set_text(">")
+
+btn2 = lv.btn(scr2)
+btn2.align(scr2, lv.ALIGN.IN_TOP_LEFT, 5, 5)
+label2 = lv.label(btn2)
+label2.set_text("<")
+
+style_led = lv.style_t()
+lv.style_copy(style_led, lv.style_pretty_color)
+style_led.body.radius = 800
+style_led.body.main_color = lv.color_make(0xB5, 0x0F, 0x04)
+style_led.body.grad_color = lv.color_make(0x50, 0x07, 0x02)
+style_led.body.border.color = lv.color_make(0xFA, 0x0F, 0x00)
+style_led.body.border.width = 3
+style_led.body.border.opa = lv.OPA._30
+style_led.body.shadow.color = lv.color_make(0xB5, 0x0F, 0x04)
+style_led.body.shadow.width = 5
+
+led1 = lv.led(scr2)
+# led1.set_style(lv.led.STYLE.MAIN, style_led)
+led1.align(slider, lv.ALIGN.OUT_RIGHT_MID, 10, 0)
+led1.set_bright(slider.get_value() * 2)
+led1.set_drag(True)
+
+
+def slider_event_cb(slider, event):
+    if event == lv.EVENT.VALUE_CHANGED:
+        led1.set_bright(slider.get_value() * 2)
+
+
+def btn1_event_cb(btn1, event):
+    if event == lv.EVENT.CLICKED:
+        lv.scr_load(scr2)
+
+
+def btn2_event_cb(btn2, event):
+    if event == lv.EVENT.CLICKED:
+        lv.scr_load(scr1)
+
+
+slider.set_event_cb(slider_event_cb)
+btn1.set_event_cb(btn1_event_cb)
+btn2.set_event_cb(btn2_event_cb)
+
+# Create styles for the keyboard
+rel_style = lv.style_t()
+pr_style = lv.style_t()
+
+lv.style_copy(rel_style, lv.style_btn_rel)
+rel_style.body.radius = 0
+rel_style.body.border.width = 1
+
+lv.style_copy(pr_style, lv.style_btn_pr)
+pr_style.body.radius = 0
+pr_style.body.border.width = 1
+
+# Create a keyboard and apply the styles
+kb = lv.kb(scr1)
+kb.set_cursor_manage(True)
+
+# Create a text area. The keyboard will write here
+ta = lv.ta(scr1)
+ta.set_width(450)
+ta.set_height(70)
+ta.align(kb, lv.ALIGN.OUT_TOP_MID, 0, -10)
+ta.set_text("")
+
+# Assign the text area to the keyboard
+kb.set_ta(ta)
+
+# Create a style for the Preloader
+style = lv.style_t()
+lv.style_copy(style, lv.style_plain)
+style.line.width = 10  # 10 px thick arc
+style.line.color = lv.color_hex3(0x258)  # Blueish arc color
+
+style.body.border.color = lv.color_hex3(0xBBB)  # Gray background color
+style.body.border.width = 0  # 11
+style.body.padding.left = 0
+
+# Create a Preloader object
+preload = lv.preload(scr2)
+preload.set_size(100, 100)
+preload.align(None, lv.ALIGN.CENTER, -5, 5)
+# preload.set_style(lv.preload.STYLE.MAIN, style)
+preload.set_type(lv.preload.TYPE.FILLSPIN_ARC)
