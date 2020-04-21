10dc8b52be65e820d53828a526d09d3246c28c6e
prolomb <prospero.lombardi@polimi.it>
2020-04-21 11:27:20 +0200

    lv_conf.h restored

--- lv_conf.h
+++ lv_conf.h
@@ -17,7 +17,7 @@
 
 /* Maximal horizontal and vertical resolution to support by the library.*/
 #define LV_HOR_RES_MAX          (480)
-#define LV_VER_RES_MAX          (272)	//(320)
+#define LV_VER_RES_MAX          (320)
 
 /* Color depth:
  * - 1:  1 byte per pixel
@@ -56,7 +56,7 @@
 /* Dot Per Inch: used to initialize default sizes.
  * E.g. a button with width = LV_DPI / 2 -> half inch wide
  * (Not so important, you can adjust it to modify default sizes and spaces)*/
-#define LV_DPI              60     /*[px]*/
+#define LV_DPI              100     /*[px]*/
 
 /* Type of coordinates. Should be `int16_t` (or `int32_t` for extreme cases) */
 typedef int16_t lv_coord_t;
