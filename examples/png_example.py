ea77fe68000946b5a8fa27597bce5ce49b558b24
Amir Gonnen <amirgonnen@gmail.com>
2020-08-21 01:32:48 +0300

    Update lvgl. Add PNG example

---
+++ examples/png_example.py
@@ -0,0 +1,39 @@
+import lvgl as lv
+from imagetools import get_png_info, open_png
+from ili9341 import ili9341
+from xpt2046 import xpt2046
+
+# Initialize display and touch panel
+
+disp = ili9341(dc=32, cs=33, power=-1, backlight=-1)
+touch = xpt2046()
+
+# Register PNG image decoder
+decoder = lv.img.decoder_create()
+decoder.info_cb = get_png_info
+decoder.open_cb = open_png
+
+# Create a screen with a draggable image
+
+with open('png_decoder_test.png','rb') as f:
+  png_data = f.read()
+
+png_img_dsc = lv.img_dsc_t({
+    'data_size': len(png_data),
+    'data': png_data 
+})
+
+scr = lv.scr_act()
+
+# Create an image on the left using the decoder
+
+# lv.img.cache_set_size(2)
+img1 = lv.img(scr)
+img1.align(scr, lv.ALIGN.CENTER, 0, -20)
+img1.set_src(png_img_dsc)
+img1.set_drag(True)
+
+img2 = lv.img(scr)
+img2.set_src(lv.SYMBOL.OK + "Accept")
+img2.align(img1, lv.ALIGN.OUT_BOTTOM_MID, 0, 20)
+
