ec7249fc6b510849ee2764e68abb014d0bdb9fac
Amir Gonnen <amirgonnen@gmail.com>
2020-12-23 00:42:09 +0200

    gen_mpy.py: Add convertors for 64bit integers.
    Fixes #113

--- gen/gen_mpy.py
+++ gen/gen_mpy.py
@@ -456,21 +456,27 @@ mp_to_lv = {
     'uint8_t'                   : '(uint8_t)mp_obj_get_int',
     'uint16_t'                  : '(uint16_t)mp_obj_get_int',
     'uint32_t'                  : '(uint32_t)mp_obj_get_int',
+    'uint64_t'                  : '(uint64_t)mp_obj_get_ull',
     'unsigned'                  : '(unsigned)mp_obj_get_int',
     'unsigned int'              : '(unsigned int)mp_obj_get_int',
     'unsigned char'             : '(unsigned char)mp_obj_get_int',
     'unsigned short'            : '(unsigned short)mp_obj_get_int',
     'unsigned long'             : '(unsigned long)mp_obj_get_int',
     'unsigned long int'         : '(unsigned long int)mp_obj_get_int',
+    'unsigned long long'        : '(unsigned long long)mp_obj_get_ull',
+    'unsigned long long int'    : '(unsigned long long int)mp_obj_get_ull',
     'int8_t'                    : '(int8_t)mp_obj_get_int',
     'int16_t'                   : '(int16_t)mp_obj_get_int',
     'int32_t'                   : '(int32_t)mp_obj_get_int',
+    'int64_t'                   : '(int64_t)mp_obj_get_ull',
     'size_t'                    : '(size_t)mp_obj_get_int',
     'int'                       : '(int)mp_obj_get_int',
     'char'                      : '(char)mp_obj_get_int',
     'short'                     : '(short)mp_obj_get_int',
     'long'                      : '(long)mp_obj_get_int',
     'long int'                  : '(long int)mp_obj_get_int',
+    'long long'                 : '(long long)mp_obj_get_ull',
+    'long long int'             : '(long long int)mp_obj_get_ull',
     'float'                     : 'mp_obj_get_float',
 }
 
@@ -490,21 +496,27 @@ lv_to_mp = {
     'uint8_t'                   : 'mp_obj_new_int_from_uint',
     'uint16_t'                  : 'mp_obj_new_int_from_uint',
     'uint32_t'                  : 'mp_obj_new_int_from_uint',
+    'uint64_t'                  : 'mp_obj_new_int_from_ull',
     'unsigned'                  : 'mp_obj_new_int_from_uint',
     'unsigned int'              : 'mp_obj_new_int_from_uint',
     'unsigned char'             : 'mp_obj_new_int_from_uint',
     'unsigned short'            : 'mp_obj_new_int_from_uint',
     'unsigned long'             : 'mp_obj_new_int_from_uint',
     'unsigned long int'         : 'mp_obj_new_int_from_uint',
+    'unsigned long long'        : 'mp_obj_new_int_from_ull',
+    'unsigned long long int'    : 'mp_obj_new_int_from_ull',
     'int8_t'                    : 'mp_obj_new_int',
     'int16_t'                   : 'mp_obj_new_int',
     'int32_t'                   : 'mp_obj_new_int',
+    'int64_t'                   : 'mp_obj_new_int_from_ll',
     'size_t'                    : 'mp_obj_new_int_from_uint',
     'int'                       : 'mp_obj_new_int',
     'char'                      : 'mp_obj_new_int',
     'short'                     : 'mp_obj_new_int',
     'long'                      : 'mp_obj_new_int',
     'long int'                  : 'mp_obj_new_int',
+    'long long'                 : 'mp_obj_new_int_from_ll',
+    'long long int'             : 'mp_obj_new_int_from_ll',
     'float'                     : 'mp_obj_new_float',
 }
 
@@ -524,21 +536,27 @@ lv_mp_type = {
     'uint8_t'                   : 'int',
     'uint16_t'                  : 'int',
     'uint32_t'                  : 'int',
+    'uint64_t'                  : 'int',
     'unsigned'                  : 'int',
     'unsigned int'              : 'int',
     'unsigned char'             : 'int',
     'unsigned short'            : 'int',
     'unsigned long'             : 'int',
     'unsigned long int'         : 'int',
+    'unsigned long long'        : 'int',
+    'unsigned long long int'    : 'int',
     'int8_t'                    : 'int',
     'int16_t'                   : 'int',
     'int32_t'                   : 'int',
+    'int64_t'                   : 'int',
     'size_t'                    : 'int',
     'int'                       : 'int',
     'char'                      : 'int',
     'short'                     : 'int',
     'long'                      : 'int',
     'long int'                  : 'int',
+    'long long'                 : 'int',
+    'long long int'             : 'int',
     'void'                      : 'NoneType',
     'float'                     : 'float',
 }
@@ -569,6 +587,7 @@ print ("""
 #include <stdlib.h>
 #include <string.h>
 #include "py/obj.h"
+#include "py/objint.h"
 #include "py/objstr.h"
 #include "py/runtime.h"
 #include "py/binary.h"
@@ -1152,6 +1171,19 @@ STATIC mp_obj_t mp_lv_funcptr(const mp_lv_obj_fun_builtin_var_t *mp_fun, void *l
     *funcptr = *mp_fun;
     funcptr->lv_fun = lv_fun;
     return MP_OBJ_FROM_PTR(funcptr);
+}
+
+// Missing implementation for 64bit integer conversion
+
+STATIC unsigned long long mp_obj_get_ull(mp_obj_t obj)
+{
+    if (mp_obj_is_small_int(obj))
+        return MP_OBJ_SMALL_INT_VALUE(obj);
+
+    unsigned long long val = 0;
+    bool big_endian = !(__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__);
+    mp_obj_int_to_bytes_impl(obj, big_endian, sizeof(val), (byte*)&val);
+    return val;
 }
 
 """)
