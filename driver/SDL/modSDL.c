e0c783b57e8070dc37e82705cf7913da6c7f2867
rockindy <yuming.chang@iot-hmi.com>
2021-03-30 04:53:24 +0800

    Make lvgl tick more accurate. (rebase to master) (#130)
    Make lvgl tick more accurate.

--- driver/SDL/modSDL.c
+++ driver/SDL/modSDL.c
@@ -40,9 +40,20 @@ STATIC int tick_thread(void * data)
 {
     (void)data;
 
+    Uint64 pfreq = SDL_GetPerformanceFrequency();
+    Uint64 tick_ms = (pfreq / 1000) * LV_TICK_RATE;
+    Uint64 delta, acc = 0;
+
     while(monitor_active()) {
+        delta = SDL_GetPerformanceCounter();
         SDL_Delay(LV_TICK_RATE);   /*Sleep for LV_TICK_RATE millisecond*/
+        delta = SDL_GetPerformanceCounter() - delta;
+        acc += delta - tick_ms;
         lv_tick_inc(LV_TICK_RATE); /*Tell LittelvGL that LV_TICK_RATE milliseconds were elapsed*/
+        if (acc >= tick_ms) {
+            lv_tick_inc(LV_TICK_RATE);
+            acc -= tick_ms;
+        }
         mp_sched_schedule((mp_obj_t)&mp_lv_task_handler_obj, mp_const_none);
         pthread_kill(mp_thread, SIGUSR1); // interrupt REPL blocking input. See handle_sigusr1
     }
