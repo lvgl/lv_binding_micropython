dea74abfcf83d20c9d4ae9dd473c7d85e071a367
Amir Gonnen <amirgonnen@gmail.com>
2021-07-08 12:28:20 +0300

    Update run.sh
    Temporarily exclude advanced_demo.py until SIGSEGV is resolved
    
    Example for the problem:
    https://github.com/lvgl/lv_binding_micropython/runs/3017175341
    
    Related: https://github.com/lvgl/lv_binding_micropython/issues/164

--- tests/run.sh
+++ tests/run.sh
@@ -5,7 +5,7 @@ set -e # Exit on error
 SCRIPT_PATH="`dirname \"$0\"`"
 
 TEST_PATH="$SCRIPT_PATH/../examples/*.py $SCRIPT_PATH/../lvgl/examples/"
-EXCLUDE_PATH="$SCRIPT_PATH/../examples/fb_test.py $SCRIPT_PATH/../examples/uasyncio*.py"
+EXCLUDE_PATH="$SCRIPT_PATH/../examples/fb_test.py $SCRIPT_PATH/../examples/uasyncio*.py $SCRIPT_PATH/../examples/advanced_demo.py"
 
 EXCLUDE_FINDEXP=$(echo $EXCLUDE_PATH | sed "s/^\|[[:space:]]/ -and -not -path /g")
 
