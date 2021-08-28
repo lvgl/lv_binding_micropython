efb644b83fb360a674a19cc56e46c4f5193c781f
Chris Mumford <cmumford@cmumford.com>
2021-08-27 19:08:06 -0700

    Run tests in parallel.

--- tests/run.sh
+++ tests/run.sh
@@ -4,11 +4,13 @@ set -e # Exit on error
 
 SCRIPT_PATH="`dirname \"$0\"`"
 
+NUMCPUS=`grep -c ^processor /proc/cpuinfo`
+
 TEST_PATH="$SCRIPT_PATH/../examples/*.py $SCRIPT_PATH/../lvgl/examples/"
 EXCLUDE_PATH="$SCRIPT_PATH/../examples/fb_test.py $SCRIPT_PATH/../examples/uasyncio*.py"
 
 EXCLUDE_FINDEXP=$(echo $EXCLUDE_PATH | sed "s/^\|[[:space:]]/ -and -not -path /g")
 
 find $TEST_PATH -name "*.py" $EXCLUDE_FINDEXP |\
-   xargs -I {} timeout 5m catchsegv $SCRIPT_PATH/../../../ports/unix/micropython-dev $SCRIPT_PATH/run_test.py {}
+   xargs --max-args=1 --max-procs $NUMCPUS -I {} timeout 5m catchsegv $SCRIPT_PATH/../../../ports/unix/micropython-dev $SCRIPT_PATH/run_test.py {}
 
