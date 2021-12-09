ebc60c2e83da32a2e5575aaf6a62aafd7bda0bd9
russhughes <russ@owt.com>
2021-12-08 17:51:05 -0800

    remove config modules for boards and lv_examples

--- boards/lilygo_tdisplay/tdisplay_landscape.py
+++
@@ -1,3 +0,0 @@
-from ili9XXX import st7789, LANDSCAPE
-
-disp = st7789(width=240, height=135, rot=LANDSCAPE, start_x=40, start_y=53, initialize=False)
--- boards/lilygo_tdisplay/tdisplay_portrait.py
+++
@@ -1,3 +0,0 @@
-from ili9XXX import st7789, PORTRAIT
-
-disp = st7789(width=135, height=240, rot=PORTRAIT, start_x=52, start_y=40, initialize=False)
