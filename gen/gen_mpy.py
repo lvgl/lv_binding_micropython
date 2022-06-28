865dae94de42be212f76e21508479c7171f7f690
Amir Gonnen <amirgonnen@gmail.com>
2022-06-29 00:00:55 +0300

    gen_mpy.py: MP_REGISTER_MODULE bugfix

--- gen/gen_mpy.py
+++ gen/gen_mpy.py
@@ -2817,7 +2817,7 @@ const mp_obj_module_t mp_module_{module_name} = {{
     .globals = (mp_obj_dict_t*)&mp_module_{module_name}_globals
 }};
 
-MP_REGISTER_MODULE(MP_QSTR_lvgl, mp_module_{module_name});
+MP_REGISTER_MODULE(MP_QSTR_{module_name}, mp_module_{module_name});
 
 """.format(
         module_name = module_name,
