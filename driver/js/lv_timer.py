405cac997c4ae386cf13c208d64c9f647bfc7dd7
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-04-07 11:39:48 -0400

    Implement Timer for JS port

--- driver/js/lv_timer.py
+++ driver/js/lv_timer.py
@@ -1,13 +1,9 @@
-# Stub Timer implementation for the JS port.
+# Timer implementation for the JS port.
 #
-# This does absolutely nothing beyond provide a class, so it will
-# not be suitable for any application which needs a timer.
-#
-# MIT license; Copyright (c) 2021 embeddedt
+# Idea by X-Ryl669: https://github.com/lvgl/lvgl/issues/4011#issuecomment-1483094051
 
-
+import jswindow
 class Timer:
-
     PERIODIC = 0
     ONE_SHOT = 1
 
@@ -15,7 +11,14 @@ class Timer:
         pass
 
     def init(self, mode=PERIODIC, period=-1, callback=None):
-        pass
+        self.mode = mode
+        if mode == PERIODIC:
+           self.id = jswindow.setInterval(callback, period)
+        else:
+           self.id = jswindow.setTimeout(callback, period)
 
     def deinit(self):
-        pass
+        if self.mode == PERIODIC:
+           jswindow.clearInterval(self.id)
+        else
+           jswindow.clearTimeout(self.id)
