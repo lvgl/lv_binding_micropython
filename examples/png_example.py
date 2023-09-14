d45e0d1a50ef0af7b9b0d57a5f482acd0f1c4130
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2023-09-15 00:56:20 +0200

    update lvgl and fix png example

--- examples/png_example.py
+++ examples/png_example.py
@@ -17,7 +17,7 @@ lv.init()
 driver = display_driver_utils.driver()
 
 scr = lv.scr_act()
-lv.image.cache_set_size(2)
+
 try:
     script_path = __file__[:__file__.rfind('/')] if __file__.find('/') >= 0 else '.'
 except NameError: 
