01045cb4ea9c2d8f756f14deeb6244b209e21784
Gabor Peresztegi <gabor.p1983@gmail.com>
2024-01-18 22:34:53 +0100

    Fix examples to align to LVGL changes

--- examples/advanced_demo.py
+++ examples/advanced_demo.py
@@ -202,7 +202,7 @@ class Page_Simple:
         self.counter_label.set_text(str(self.counter))
 
 class Anim(lv.anim_t):
-    def __init__(self, obj, val, size, exec_cb, path_cb, time=500, playback=False, ready_cb=None):
+    def __init__(self, obj, val, size, exec_cb, path_cb, time=500, playback=False, completed_cb=None):
         super().__init__()
         self.init()
         self.set_time(time)
@@ -214,8 +214,8 @@ class Anim(lv.anim_t):
         self.set_path_cb(path_cb)
         if playback:
             self.set_playback(0)
-        if ready_cb:
-            self.set_ready_cb(ready_cb)
+        if completed_cb:
+            self.set_completed_cb(completed_cb)
         self.start()
         
 
@@ -236,7 +236,7 @@ class AnimatedChart(lv.chart):
             self.size,
             lambda a, val: self.set_range(self.AXIS.PRIMARY_Y, 0, val),
             lv.anim_t.path_ease_in,
-            ready_cb=lambda a:self.anim_phase2(),
+            completed_cb=lambda a:self.anim_phase2(),
             time=(self.max * self.factor) // 100,
         )
 
@@ -247,7 +247,7 @@ class AnimatedChart(lv.chart):
             -self.size,
             lambda a, val: self.set_range(self.AXIS.PRIMARY_Y, 0, val),
             lv.anim_t.path_ease_out,
-            ready_cb=lambda a:self.anim_phase1(),
+            completed_cb=lambda a:self.anim_phase1(),
             time=(self.min * self.factor) // 100,
         )
 class Page_Text:
