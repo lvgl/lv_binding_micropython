926d5ea542133c2a7b1827cc11e402e83afb48da
Carlosgg <carlosgilglez@gmail.com>
2024-04-25 22:43:38 +0100

    esp32: Enable lv_binding_micropython as user C mod.

--- micropython.cmake
+++ micropython.cmake
@@ -1,8 +1,45 @@
-# For detauils see: https://docs.micropython.org/en/latest/develop/cmodules.html
+# This file is to be given as "make USER_C_MODULES=..." when building Micropython port
 
-set(LVGL_ROOT_DIR ${CMAKE_CURRENT_LIST_DIR})
+# Include LVGL component, ignore KCONFIG
 
-include(${CMAKE_CURRENT_LIST_DIR}/env_support/cmake/micropython.cmake)
+idf_build_set_property(LV_MICROPYTHON 1)
+idf_build_component(${CMAKE_CURRENT_LIST_DIR}/lvgl)
+idf_build_set_property(COMPILE_DEFINITIONS "-DLV_KCONFIG_IGNORE" APPEND)
+separate_arguments(LV_CFLAGS_ENV UNIX_COMMAND $ENV{LV_CFLAGS})
+idf_build_set_property(COMPILE_DEFINITIONS "${LV_CFLAGS}" APPEND)
+idf_build_set_property(COMPILE_OPTIONS "-Wno-unused-function" APPEND)
+idf_build_set_property(SRCS "${LV_SRC}" APPEND)
+idf_build_set_property(INCLUDE_DIRS "${LV_INCLUDE}" APPEND)
 
-# Link our INTERFACE library to the usermod target.
-target_link_libraries(usermod INTERFACE lvgl_interface)
+include(${CMAKE_CURRENT_LIST_DIR}/mkrules_usermod.cmake)
+
+# Add lv_bindings rules
+
+all_lv_bindings()
+
+
+# # # make usermod (target declared by Micropython for all user compiled modules) link to bindings
+# # # this way the bindings (and transitively lvgl_interface) get proper compilation flags
+# target_link_libraries(usermod INTERFACE usermod_lvgl)
+
+file(GLOB_RECURSE SOURCES ${CMAKE_CURRENT_LIST_DIR}/lvgl/src/*.c)
+
+add_library(lvgl_interface INTERFACE)
+
+target_sources(lvgl_interface INTERFACE ${SOURCES})
+target_compile_options(lvgl_interface INTERFACE ${LV_CFLAGS})
+
+# # lvgl bindings target (the mpy module)
+
+add_library(usermod_lvgl INTERFACE)
+target_sources(usermod_lvgl INTERFACE ${LV_SRC})
+target_include_directories(usermod_lvgl INTERFACE ${LV_INCLUDE})
+
+
+file(WRITE ${LV_MP} "")
+
+target_link_libraries(usermod_lvgl INTERFACE lvgl_interface)
+
+# # # make usermod (target declared by Micropython for all user compiled modules) link to bindings
+# # # this way the bindings (and transitively lvgl_interface) get proper compilation flags
+target_link_libraries(usermod INTERFACE usermod_lvgl)
