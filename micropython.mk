84f1dd4701a74721d4b71e5049a7d814f66c31cf
Carlosgg <carlosgilglez@gmail.com>
2025-01-05 18:43:20 +0000

    fix(makefile): use CLFAGS_USERMOD

--- micropython.mk
+++ micropython.mk
@@ -1,46 +1,46 @@
 
 ################################################################################
 # LVGL unix optional libraries
-# Update CFLAGS_EXTMOD and LDFLAGS_EXTMOD for LVGL extenral library,
+# Update CFLAGS_USERMOD and LDFLAGS_USERMOD for LVGL extenral library,
 # but do that only on the unix port, for unix specific dependencies
 ifeq ($(notdir $(CURDIR)),unix)
 ifneq ($(UNAME_S),Darwin)
-CFLAGS_EXTMOD += -DMICROPY_FB=1
+CFLAGS_USERMOD += -DMICROPY_FB=1
 endif
 
-SDL_CFLAGS_EXTMOD :=  $(shell pkg-config --silence-errors --cflags sdl2)
-SDL_LDFLAGS_EXTMOD := $(shell pkg-config --silence-errors --libs   sdl2)
-ifneq ($(SDL_LDFLAGS_EXTMOD),)
-CFLAGS_EXTMOD += $(SDL_CFLAGS_EXTMOD) -DMICROPY_SDL=1
-LDFLAGS_EXTMOD += $(SDL_LDFLAGS_EXTMOD)
+SDL_CFLAGS_USERMOD :=  $(shell pkg-config --silence-errors --cflags sdl2)
+SDL_LDFLAGS_USERMOD := $(shell pkg-config --silence-errors --libs   sdl2)
+ifneq ($(SDL_LDFLAGS_USERMOD),)
+CFLAGS_USERMOD += $(SDL_CFLAGS_USERMOD) -DMICROPY_SDL=1
+LDFLAGS_USERMOD += $(SDL_LDFLAGS_USERMOD)
 endif
 
 # Avoid including unwanted local headers other than sdl2 
 ifeq ($(UNAME_S),Darwin)
-CFLAGS_EXTMOD:=$(filter-out -I/usr/local/include,$(CFLAGS_EXTMOD))
+CFLAGS_USERMOD:=$(filter-out -I/usr/local/include,$(CFLAGS_USERMOD))
 endif
 
-RLOTTIE_CFLAGS_EXTMOD :=  $(shell pkg-config --silence-errors --cflags rlottie)
-RLOTTIE_LDFLAGS_EXTMOD := $(shell pkg-config --silence-errors --libs   rlottie)
-ifneq ($(RLOTTIE_LDFLAGS_EXTMOD),)
-CFLAGS_EXTMOD += $(RLOTTIE_CFLAGS_EXTMOD) -DMICROPY_RLOTTIE=1
-LDFLAGS_EXTMOD += $(RLOTTIE_LDFLAGS_EXTMOD)
+RLOTTIE_CFLAGS_USERMOD :=  $(shell pkg-config --silence-errors --cflags rlottie)
+RLOTTIE_LDFLAGS_USERMOD := $(shell pkg-config --silence-errors --libs   rlottie)
+ifneq ($(RLOTTIE_LDFLAGS_USERMOD),)
+CFLAGS_USERMOD += $(RLOTTIE_CFLAGS_USERMOD) -DMICROPY_RLOTTIE=1
+LDFLAGS_USERMOD += $(RLOTTIE_LDFLAGS_USERMOD)
 endif
 
-FREETYPE_CFLAGS_EXTMOD :=  $(shell pkg-config --silence-errors --cflags freetype2)
-FREETYPE_LDFLAGS_EXTMOD := $(shell pkg-config --silence-errors --libs   freetype2)
-ifneq ($(FREETYPE_LDFLAGS_EXTMOD),)
-CFLAGS_EXTMOD += $(FREETYPE_CFLAGS_EXTMOD) -DMICROPY_FREETYPE=1
-LDFLAGS_EXTMOD += $(FREETYPE_LDFLAGS_EXTMOD)
+FREETYPE_CFLAGS_USERMOD :=  $(shell pkg-config --silence-errors --cflags freetype2)
+FREETYPE_LDFLAGS_USERMOD := $(shell pkg-config --silence-errors --libs   freetype2)
+ifneq ($(FREETYPE_LDFLAGS_USERMOD),)
+CFLAGS_USERMOD += $(FREETYPE_CFLAGS_USERMOD) -DMICROPY_FREETYPE=1
+LDFLAGS_USERMOD += $(FREETYPE_LDFLAGS_USERMOD)
 endif
 
 # Enable FFMPEG
 # FFMPEG_LIBS := libavformat libavcodec libswscale libavutil
-# FFMPEG_CFLAGS_EXTMOD :=  $(shell pkg-config --silence-errors --cflags $(FFMPEG_LIBS))
-# FFMPEG_LDFLAGS_EXTMOD := $(shell pkg-config --silence-errors --libs   $(FFMPEG_LIBS))
-# ifneq ($(FFMPEG_LDFLAGS_EXTMOD),)
-# CFLAGS_EXTMOD += $(FFMPEG_CFLAGS_EXTMOD) -DMICROPY_FFMPEG=1
-# LDFLAGS_EXTMOD += $(FFMPEG_LDFLAGS_EXTMOD)
+# FFMPEG_CFLAGS_USERMOD :=  $(shell pkg-config --silence-errors --cflags $(FFMPEG_LIBS))
+# FFMPEG_LDFLAGS_USERMOD := $(shell pkg-config --silence-errors --libs   $(FFMPEG_LIBS))
+# ifneq ($(FFMPEG_LDFLAGS_USERMOD),)
+# CFLAGS_USERMOD += $(FFMPEG_CFLAGS_USERMOD) -DMICROPY_FFMPEG=1
+# LDFLAGS_USERMOD += $(FFMPEG_LDFLAGS_USERMOD)
 # endif
 
 endif
@@ -66,23 +66,25 @@ ALL_LVGL_SRC = $(shell find $(LVGL_DIR) -type f -name '*.h') $(LV_CONF_PATH)
 LVGL_PP = $(BUILD)/lvgl/lvgl.pp.c
 LVGL_MPY = $(BUILD)/lvgl/lv_mpy.c
 LVGL_MPY_METADATA = $(BUILD)/lvgl/lv_mpy.json
-CFLAGS_EXTMOD += $(LV_CFLAGS) 
-CFLAGS_EXTMOD += -DLV_CONF_PATH=$(LV_CONF_PATH)
+CFLAGS_USERMOD += $(LV_CFLAGS)
+
+# MAKE SURE LV_CONF_PATH is a STRING
+CFLAGS_USERMOD += -DLV_CONF_PATH=$(LV_CONF_PATH)
 
 
 # CFLAGS DEBUG
-$(info CFLAGS_EXTMOD is $(CFLAGS_EXTMOD))
+$(info CFLAGS_USERMOD is $(CFLAGS_USERMOD))
 
 $(LVGL_MPY): $(ALL_LVGL_SRC) $(LVGL_BINDING_DIR)/gen/gen_mpy.py 
 	$(ECHO) "LVGL-GEN $@"
 	$(Q)mkdir -p $(dir $@)
-	$(Q)$(CPP) $(CFLAGS_EXTMOD) -DPYCPARSER -x c -I $(LVGL_BINDING_DIR)/pycparser/utils/fake_libc_include $(INC) $(LVGL_DIR)/lvgl.h > $(LVGL_PP)
+	$(Q)$(CPP) $(CFLAGS_USERMOD) -DPYCPARSER -x c -I $(LVGL_BINDING_DIR)/pycparser/utils/fake_libc_include $(INC) $(LVGL_DIR)/lvgl.h > $(LVGL_PP)
 	$(Q)$(PYTHON) $(LVGL_BINDING_DIR)/gen/gen_mpy.py -M lvgl -MP lv -MD $(LVGL_MPY_METADATA) -E $(LVGL_PP) $(LVGL_DIR)/lvgl.h > $@
 
 .PHONY: LVGL_MPY
 LVGL_MPY: $(LVGL_MPY)
 
-CFLAGS_EXTMOD += -Wno-unused-function
+CFLAGS_USERMOD += -Wno-unused-function
 CFLAGS_EXTRA += -Wno-unused-function
-SRC_THIRDPARTY_C += $(subst $(TOP)/,,$(shell find $(LVGL_DIR)/src $(LVGL_DIR)/examples $(LVGL_GENERIC_DRV_DIR) -type f -name "*.c"))
-SRC_EXTMOD_C += $(LVGL_MPY)
+SRC_USERMOD_LIB_C += $(subst $(TOP)/,,$(shell find $(LVGL_DIR)/src $(LVGL_DIR)/examples $(LVGL_GENERIC_DRV_DIR) -type f -name "*.c"))
+SRC_USERMOD_C += $(LVGL_MPY)
