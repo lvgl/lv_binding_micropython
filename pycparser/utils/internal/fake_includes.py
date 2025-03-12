c021cd33d3270af0d97597ddb1ff2040732108d0
diskman88 <diskman88@163.com>
2025-03-12 20:06:25 +0800

    把lvgl pycparser从子模块变为文件夹:

---
+++ pycparser/utils/internal/fake_includes.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+import os.path
+
+for cur_path, dirs, files in os.walk('.'):
+    if cur_path == '.':
+        for f in files:
+            if f.endswith('.h'):
+                print(f)
+                fo = open(f, 'w')
+                fo.write('#include "_fake_defines.h"\n')
+                fo.write('#include "_fake_typedefs.h"\n')
+                fo.close()
+
+
