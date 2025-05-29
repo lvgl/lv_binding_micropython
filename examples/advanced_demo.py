f51f81f24ec796a4804a6e6cce4f5f1236ccfc8b
Carlosgg <carlosgilglez@gmail.com>
2025-05-30 00:24:12 +0100

    (fix) unix port workflow

--- examples/advanced_demo.py
+++ examples/advanced_demo.py
@@ -1,15 +1,18 @@
 import usys as sys
-sys.path.append('') # See: https://github.com/micropython/micropython/issues/6419
+
+sys.path.append("")  # See: https://github.com/micropython/micropython/issues/6419
 
 # See: https://pymotw.com/2/sys/tracing.html
+
 
 def mp_trace(frame, event, arg):
     co = frame.f_code
     func_name = co.co_name
     func_line_no = frame.f_lineno
     func_filename = co.co_filename
-    print('[%s] [%s] %s:%s' % (event, func_name, func_filename, func_line_no))
+    print("[%s] [%s] %s:%s" % (event, func_name, func_filename, func_line_no))
     return mp_trace
+
 
 # sys.settrace(mp_trace)
 
@@ -24,6 +27,7 @@ lv.init()
 # Styles
 ##############################################################################
 
+
 class ColorStyle(lv.style_t):
     def __init__(self, color):
         super().__init__()
@@ -34,6 +38,7 @@ class ColorStyle(lv.style_t):
         self.set_bg_main_stop(0)
         self.set_bg_grad_stop(128)
 
+
 class ShadowStyle(lv.style_t):
     def __init__(self):
         super().__init__()
@@ -43,6 +48,7 @@ class ShadowStyle(lv.style_t):
         self.set_shadow_offset_x(5)
         self.set_shadow_offset_y(3)
         self.set_shadow_spread(0)
+
 
 # A square button with a shadow when not pressed
 class ButtonStyle(lv.style_t):
@@ -56,6 +62,7 @@ class ButtonStyle(lv.style_t):
         self.set_shadow_offset_y(lv.dpx(10))
         self.set_shadow_spread(0)
 
+
 class ButtonPressedStyle(lv.style_t):
     def __init__(self):
         super().__init__()
@@ -67,8 +74,8 @@ class ButtonPressedStyle(lv.style_t):
 # Themes
 ##############################################################################
 
-class AdvancedDemoTheme(lv.theme_t):
 
+class AdvancedDemoTheme(lv.theme_t):
     def __init__(self):
         super().__init__()
         self.button_style = ButtonStyle()
@@ -85,15 +92,17 @@ class AdvancedDemoTheme(lv.theme_t):
 
         # Activate this theme on default display
         lv.display_get_default().set_theme(self)
-    
+
     def apply(self, theme, obj):
         if obj.get_class() == lv.button_class:
             obj.add_style(self.button_style, lv.PART.MAIN)
             obj.add_style(self.button_pressed_style, lv.PART.MAIN | lv.STATE.PRESSED)
 
+
 ##############################################################################
 
 member_name_cache = {}
+
 
 def get_member_name(obj, value):
     try:
@@ -118,17 +127,21 @@ class SymbolButton(lv.button):
         self.label = lv.label(self)
         self.label.set_text(text)
         self.set_flex_flow(lv.FLEX_FLOW.COLUMN)
-        self.set_flex_align(lv.FLEX_ALIGN.SPACE_EVENLY, lv.FLEX_ALIGN.CENTER, lv.FLEX_ALIGN.CENTER)
+        self.set_flex_align(
+            lv.FLEX_ALIGN.SPACE_EVENLY, lv.FLEX_ALIGN.CENTER, lv.FLEX_ALIGN.CENTER
+        )
 
 
 class Page_Buttons:
     def __init__(self, app, page):
         self.app = app
         self.page = page
-        self.button_event_count = {'Play': 0, 'Pause': 0}
+        self.button_event_count = {"Play": 0, "Pause": 0}
 
         self.page.set_flex_flow(lv.FLEX_FLOW.ROW)
-        self.page.set_flex_align(lv.FLEX_ALIGN.SPACE_EVENLY, lv.FLEX_ALIGN.CENTER, lv.FLEX_ALIGN.START)
+        self.page.set_flex_align(
+            lv.FLEX_ALIGN.SPACE_EVENLY, lv.FLEX_ALIGN.CENTER, lv.FLEX_ALIGN.START
+        )
 
         self.button1 = SymbolButton(page, lv.SYMBOL.PLAY, "Play")
         self.button1.set_size(80, 80)
@@ -143,11 +156,19 @@ class Page_Buttons:
         def button_cb(event, name):
             self.button_event_count[name] += 1
             event_name = get_member_name(lv.EVENT, event.code)
-            if all((not event_name.startswith(s)) for s in ['DRAW', 'GET', 'STYLE', 'REFR']):
-                self.label.set_text('[%d] %s %s' % (self.button_event_count[name], name, event_name))
+            if all(
+                (not event_name.startswith(s)) for s in ["DRAW", "GET", "STYLE", "REFR"]
+            ):
+                self.label.set_text(
+                    "[%d] %s %s" % (self.button_event_count[name], name, event_name)
+                )
 
-        for button, name in [(self.button1, 'Play'), (self.button2, 'Pause')]:
-            button.add_event_cb(lambda event, button_name=name: button_cb(event, button_name), lv.EVENT.ALL, None)
+        for button, name in [(self.button1, "Play"), (self.button2, "Pause")]:
+            button.add_event_cb(
+                lambda event, button_name=name: button_cb(event, button_name),
+                lv.EVENT.ALL,
+                None,
+            )
 
 
 class Page_Simple:
@@ -157,7 +178,9 @@ class Page_Simple:
         self.test_events = []
 
         self.page.set_flex_flow(lv.FLEX_FLOW.COLUMN)
-        self.page.set_flex_align(lv.FLEX_ALIGN.SPACE_EVENLY, lv.FLEX_ALIGN.CENTER, lv.FLEX_ALIGN.CENTER)
+        self.page.set_flex_align(
+            lv.FLEX_ALIGN.SPACE_EVENLY, lv.FLEX_ALIGN.CENTER, lv.FLEX_ALIGN.CENTER
+        )
 
         # slider
         self.slider = lv.slider(page)
@@ -167,20 +190,24 @@ class Page_Simple:
         self.on_slider_changed(None)
 
         # style selector
-        self.styles = [('Gray', ColorStyle(0xCCC)),
-                       ('Red', ColorStyle(0xF00)), 
-                       ('Green',ColorStyle(0x0F0)),
-                       ('Blue', ColorStyle(0x00F))] 
-    
+        self.styles = [
+            ("Gray", ColorStyle(0xCCC)),
+            ("Red", ColorStyle(0xF00)),
+            ("Green", ColorStyle(0x0F0)),
+            ("Blue", ColorStyle(0x00F)),
+        ]
+
         self.style_selector = lv.dropdown(page)
         self.style_selector.add_style(ShadowStyle(), lv.PART.MAIN)
         self.style_selector.align(lv.ALIGN.OUT_BOTTOM_LEFT, 0, 40)
-        self.style_selector.set_options('\n'.join(x[0] for x in self.styles))
-        self.style_selector.add_event_cb(self.on_style_selector_changed, lv.EVENT.VALUE_CHANGED, None)
+        self.style_selector.set_options("\n".join(x[0] for x in self.styles))
+        self.style_selector.add_event_cb(
+            self.on_style_selector_changed, lv.EVENT.VALUE_CHANGED, None
+        )
 
         # counter button
         self.counter_button = lv.button(page)
-        self.counter_button.set_size(80,80)
+        self.counter_button.set_size(80, 80)
         self.counter_label = lv.label(self.counter_button)
         self.counter_label.set_text("Count")
         self.counter_label.align(lv.ALIGN.CENTER, 0, 0)
@@ -193,7 +220,8 @@ class Page_Simple:
     def on_style_selector_changed(self, event):
         selected = self.style_selector.get_selected()
         tabview = self.app.screen_main.tabview
-        if hasattr(self, 'selected_style'): tabview.remove_style(self.selected_style, lv.PART.MAIN)
+        if hasattr(self, "selected_style"):
+            tabview.remove_style(self.selected_style, lv.PART.MAIN)
         self.selected_style = self.styles[selected][1]
         tabview.add_style(self.selected_style, lv.PART.MAIN)
 
@@ -201,11 +229,22 @@ class Page_Simple:
         self.counter += 1
         self.counter_label.set_text(str(self.counter))
 
+
 class Anim(lv.anim_t):
-    def __init__(self, obj, val, size, exec_cb, path_cb, time=500, playback=False, completed_cb=None):
+    def __init__(
+        self,
+        obj,
+        val,
+        size,
+        exec_cb,
+        path_cb,
+        time=500,
+        playback=False,
+        completed_cb=None,
+    ):
         super().__init__()
         self.init()
-        self.set_time(time)
+        self.set_duration(time)
         self.set_values(val, val + size)
         if callable(exec_cb):
             self.set_custom_exec_cb(exec_cb)
@@ -217,7 +256,7 @@ class Anim(lv.anim_t):
         if completed_cb:
             self.set_completed_cb(completed_cb)
         self.start()
-        
+
 
 class AnimatedChart(lv.chart):
     def __init__(self, parent, val, size):
@@ -234,9 +273,9 @@ class AnimatedChart(lv.chart):
             self,
             self.val,
             self.size,
-            lambda a, val: self.set_range(self.AXIS.PRIMARY_Y, 0, val),
+            lambda a, val: self.set_axis_range(self.AXIS.PRIMARY_Y, 0, val),
             lv.anim_t.path_ease_in,
-            completed_cb=lambda a:self.anim_phase2(),
+            completed_cb=lambda a: self.anim_phase2(),
             time=(self.max * self.factor) // 100,
         )
 
@@ -245,39 +284,50 @@ class AnimatedChart(lv.chart):
             self,
             self.val + self.size,
             -self.size,
-            lambda a, val: self.set_range(self.AXIS.PRIMARY_Y, 0, val),
+            lambda a, val: self.set_axis_range(self.AXIS.PRIMARY_Y, 0, val),
             lv.anim_t.path_ease_out,
-            completed_cb=lambda a:self.anim_phase1(),
+            completed_cb=lambda a: self.anim_phase1(),
             time=(self.min * self.factor) // 100,
         )
+
+
 class Page_Text:
     def __init__(self, app, page):
         self.app = app
         self.page = page
         self.page.set_flex_flow(lv.FLEX_FLOW.ROW)
-        self.page.set_flex_align(lv.FLEX_ALIGN.SPACE_EVENLY, lv.FLEX_ALIGN.CENTER, lv.FLEX_ALIGN.CENTER)
+        self.page.set_flex_align(
+            lv.FLEX_ALIGN.SPACE_EVENLY, lv.FLEX_ALIGN.CENTER, lv.FLEX_ALIGN.CENTER
+        )
         self.ta = lv.textarea(self.page)
         self.ta.set_height(lv.pct(100))
         self.ta.set_width(lv.pct(100))
+
 
 class Page_Chart:
     def __init__(self, app, page):
         self.app = app
         self.page = page
         self.page.set_flex_flow(lv.FLEX_FLOW.ROW)
-        self.page.set_flex_align(lv.FLEX_ALIGN.SPACE_EVENLY, lv.FLEX_ALIGN.CENTER, lv.FLEX_ALIGN.CENTER)
+        self.page.set_flex_align(
+            lv.FLEX_ALIGN.SPACE_EVENLY, lv.FLEX_ALIGN.CENTER, lv.FLEX_ALIGN.CENTER
+        )
         self.page.set_style_pad_all(10, lv.PART.MAIN)
         self.page.set_style_pad_gap(10, lv.PART.MAIN)
         self.chart = AnimatedChart(page, 100, 1000)
         self.chart.set_flex_grow(1)
         self.chart.set_height(lv.pct(100))
-        self.series1 = self.chart.add_series(lv.color_hex(0xFF0000), self.chart.AXIS.PRIMARY_Y)
+        self.series1 = self.chart.add_series(
+            lv.color_hex(0xFF0000), self.chart.AXIS.PRIMARY_Y
+        )
         self.chart.set_type(self.chart.TYPE.LINE)
         self.chart.set_style_line_width(3, lv.PART.ITEMS)
         self.chart.add_style(ColorStyle(0x055), lv.PART.ITEMS)
-        self.chart.set_range(self.chart.AXIS.PRIMARY_Y, 0, 100)
+        self.chart.set_axis_range(self.chart.AXIS.PRIMARY_Y, 0, 100)
         self.chart.set_point_count(10)
-        self.chart.set_ext_y_array(self.series1, [10, 20, 30, 20, 10, 40, 50, 90, 95, 90])
+        self.chart.set_series_ext_y_array(
+            self.series1, [10, 20, 30, 20, 10, 40, 50, 90, 95, 90]
+        )
         # self.chart.set_x_tick_texts("a\nb\nc\nd\ne", 2, lv.chart.AXIS.DRAW_LAST_TICK)
         # self.chart.set_x_tick_length(10, 5)
         # self.chart.set_y_tick_texts("1\n2\n3\n4\n5", 2, lv.chart.AXIS.DRAW_LAST_TICK)
@@ -294,6 +344,7 @@ class Page_Chart:
         self.slider.set_range(10, 200)
         self.slider.set_value(self.chart.factor, 0)
         self.slider.add_event_cb(on_slider_changed, lv.EVENT.VALUE_CHANGED, None)
+
 
 class Screen_Main(lv.obj):
     def __init__(self, app, *args, **kwds):
@@ -319,16 +370,16 @@ class AdvancedDemoApplication:
         self.mouse = lv.sdl_mouse_create()
         self.keyboard = lv.sdl_keyboard_create()
         self.keyboard.set_group(self.group)
-        
-    def init_gui_esp32(self):
 
+    def init_gui_esp32(self):
         # Initialize ILI9341 display
 
         from ili9XXX import ili9341
 
         # self.disp = ili9341(dc=32, cs=33, power=-1, backlight=-1)
-        self.disp = ili9341(mhz=20, dc=32, cs=33, power=-1, backlight=-1, hybrid=False, factor=8)
-
+        self.disp = ili9341(
+            mhz=20, dc=32, cs=33, power=-1, backlight=-1, hybrid=False, factor=8
+        )
 
         # Register raw resistive touch driver
 
@@ -357,7 +408,7 @@ class AdvancedDemoApplication:
         # Register display driver
         self.event_loop = event_loop()
         lcd.init(w=hres, h=vres)
-        
+
         draw_buf1 = lv.draw_buf_create(hres, 50, color_format, 0)
         draw_buf2 = lv.draw_buf_create(hres, 50, color_format, 0)
 
@@ -375,36 +426,46 @@ class AdvancedDemoApplication:
     def init_gui_rp2(self):
         import xpt2046
         import st77xx
-        if sys.platform!='rp2': raise ImportError('Only works on the rp2 platform.')
-        print('Using RP2 GUI')
-        spi=machine.SPI(
+
+        if sys.platform != "rp2":
+            raise ImportError("Only works on the rp2 platform.")
+        print("Using RP2 GUI")
+        spi = machine.SPI(
             1,
             baudrate=24_000_000,
             polarity=0,
             phase=0,
-            sck=machine.Pin(10,machine.Pin.OUT),
-            mosi=machine.Pin(11,machine.Pin.OUT),
-            miso=machine.Pin(12,machine.Pin.IN)
+            sck=machine.Pin(10, machine.Pin.OUT),
+            mosi=machine.Pin(11, machine.Pin.OUT),
+            miso=machine.Pin(12, machine.Pin.IN),
         )
-        self.disp=st77xx.St7789(rot=st77xx.ST77XX_INV_LANDSCAPE,res=(240,320),spi=spi,cs=9,dc=8,bl=13,rst=15,rp2_dma=None)
+        self.disp = st77xx.St7789(
+            rot=st77xx.ST77XX_INV_LANDSCAPE,
+            res=(240, 320),
+            spi=spi,
+            cs=9,
+            dc=8,
+            bl=13,
+            rst=15,
+            rp2_dma=None,
+        )
         self.disp.set_backlight(100)
-        self.touch=xpt2046.Xpt2046(spi=spi,cs=16,rot=xpt2046.XPT2046_INV_LANDSCAPE)
+        self.touch = xpt2046.Xpt2046(spi=spi, cs=16, rot=xpt2046.XPT2046_INV_LANDSCAPE)
 
     def init_gui(self):
-
         self.group = lv.group_create()
         self.group.set_default()
 
         # Identify platform and initialize it
 
         if not event_loop.is_running():
-            if sys.platform == 'rp2':
+            if sys.platform == "rp2":
                 self.init_gui_rp2()
-            elif sys.platform == 'esp32':
+            elif sys.platform == "esp32":
                 self.init_gui_esp32()
-            elif sys.platform == 'linux':
+            elif sys.platform == "linux":
                 self.init_gui_SDL()
-            elif sys.platform == 'stm32':
+            elif sys.platform == "stm32":
                 self.init_gui_stm32()
 
         # Create the main screen and load it.
@@ -419,4 +480,3 @@ app.init_gui()
 # if __name__ == '__main__':
 #    while True:
 #        pass
-    
